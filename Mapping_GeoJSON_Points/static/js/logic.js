2926d6b9a2bed4c8c3d59e72eeb9d6fb4acd2171
yycheung <yycheung@Galaxy.local>
2022-04-27 23:05:05 -0700

    updated the json url

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -51,8 +51,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/yycheung/Mapping_Earthquakes/main/majorAirports.json";
-
+let airportData = "https://raw.githubusercontent.com/yycheung/Mapping_Earthquake/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
